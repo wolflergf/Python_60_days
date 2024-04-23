84244027c6827782fddfd5dad95ade25a151b2a1
wolflergf <wolflergf@gmail.com>
2024-04-23 22:55:26 +0100

    First day completed.

---
+++ Section_02/bonus1.py
@@ -0,0 +1,2 @@
+text = input("Enter a title: ")
+print(f"There are {len(text)} characters in the title")
--- Section_02/main.py
+++ Section_02/main.py
@@ -1,3 +1,12 @@
-user_prompt = "Enter a todo item: "
-text = input(user_prompt)
-print(text)
+"""
+This will be the prompt that the user sees when they run the program
+"""
+
+USER_PROMPT = "Enter a todo item: "
+
+todo1 = input(USER_PROMPT)
+todo2 = input(USER_PROMPT)
+todo3 = input(USER_PROMPT)
+todos = [todo1, todo2, todo3]
+
+print(todos)
