01360a29ef8e0dc3dac445cd6b9ecd55e5e4ecf9
wolflergf <wolflergf@gmail.com>
2024-04-29 23:00:53 +0100

    Implemented remove function

--- Section_06/main.py
+++ Section_06/main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, edit or exit: ").capitalize().strip()
+    user_action = input("Type add, show, edit, remove or exit: ").capitalize().strip()
 
     match user_action:
         case "Add":
@@ -20,7 +20,15 @@ while True:
             new_todo = input("Enter new todo: ").title()
             todos[index] = new_todo
 
+        case "Remove":
+            for index, item in enumerate(todos):
+                print(index + 1, "-", item)
+
+            index = int(input("Number of the todo to remove: ")) - 1
+            todos.pop(index)
+
         case "Exit":
+            print("See you later!")
             break
 
         case _:
