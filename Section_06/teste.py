906cc8daf43882eeb9d8dc832924f89d64a54288
wolflergf <wolflergf@gmail.com>
2024-04-30 21:59:52 +0100

    Day 5

---
+++ Section_06/teste.py
@@ -0,0 +1,6 @@
+ips = ["100.122.133.105", "100.122.133.111"]
+
+number = (int(input("Enter the index of the IP you want: "))) - 1
+
+print("You chose {}.".format(ips[number]))
+print(f"You chose {ips[number]}.")
