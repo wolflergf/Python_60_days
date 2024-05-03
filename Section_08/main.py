097323921c76f575049b8e0224c1ada3cfd33005
wolflergf <wolflergf@gmail.com>
2024-05-03 17:58:48 +0100

    Day 7

---
+++ Section_08/main.py
@@ -0,0 +1,62 @@
+while True:
+    user_action = input("Type add, show, edit, complete or exit: ").title()
+
+    match user_action:
+        case "Add":
+            file = open("./todos.txt", "r")
+            todos = file.readlines()
+            file.close()
+
+            todo = input("Enter a todo: ").title()
+            todos.append(f"{todo}\n")
+
+            file = open("./todos.txt", "w")
+            file.writelines(todos)
+            file.close()
+
+        case "Show":
+            file = open("./todos.txt", "r")
+            todos = file.readlines()
+            file.close()
+
+            for index, todo in enumerate(todos):
+                print(f"{index + 1} - {todo.strip()}")
+
+        case "Edit":
+
+            file = open("./todos.txt", "r")
+            todos = file.readlines()
+            file.close()
+
+            for index, todo in enumerate(todos):
+                print(f"{index + 1} - {todo.strip()}")
+
+            number_to_edit = int(input("Enter the number to edit: ")) - 1
+            todos[number_to_edit] = input("Enter a new todo: ").title() + "\n"
+
+            file = open("./todos.txt", "w")
+            file.writelines(todos)
+            file.close()
+
+        case "Complete":
+
+            file = open("./todos.txt", "r")
+            todos = file.readlines()
+            file.close()
+
+            for index, todo in enumerate(todos):
+                print(f"{index + 1} - {todo.strip()}")
+
+            number_to_edit = int(input("Enter the number to edit: ")) - 1
+            todos.pop(number_to_edit)
+
+            file = open("./todos.txt", "w")
+            file.writelines(todos)
+            file.close()
+
+        case "Exit":
+            print("See you soon!")
+            break
+
+        case _:
+            print("Something is wrong!")
