698de1533d076e8607b9fe46a6fccf833a88cf90
wolflergf <wolflergf@gmail.com>
2024-05-04 23:35:17 +0100

    update

--- Section_09/main.py
+++ Section_09/main.py
@@ -5,39 +5,40 @@ while True:
         case "Add":
             todo = input("Enter a todo: ").title() + "\n"
 
-            with open("./todos.txt", "r") as file:
+            with open("./todos.txt", "r", encoding="utf-8") as file:
                 todos = file.readlines()
 
             todos.append(todo)
 
-            with open("./todos.txt", "w") as file:
+            with open("./todos.txt", "w", encoding="utf-8") as file:
                 file.writelines(todos)
 
         case "Show":
-            with open("./todos.txt", "r") as file:
+            with open("./todos.txt", "r", encoding="utf-8") as file:
                 todos = file.readlines()
 
                 for index, todo in enumerate(todos):
                     print(f"{index + 1} - {todo.strip()}")
 
         case "Edit":
-            with open("./todos.txt", "r") as file:
+            with open("./todos.txt", "r", encoding="utf-8") as file:
                 todos = file.readlines()
 
             number = int(input("Number of the todo for edit: ")) - 1
             todos[number] = input("Enter a new todo: ").title() + "\n"
 
-            with open("./todos.txt", "w") as file:
+            with open("./todos.txt", "w", encoding="utf-8") as file:
                 file.writelines(todos)
 
         case "Complete":
-            with open("./todos.txt", "r") as file:
+            with open("./todos.txt", "r", encoding="utf-8") as file:
                 todos = file.readlines()
 
             number = int(input("Number of the todo for delete: ")) - 1
+            print(f"Todo '{todos[number].strip()}' was removed form the list.")
             todos.pop(number)
 
-            with open("./todos.txt", "w") as file:
+            with open("./todos.txt", "w", encoding="utf-8") as file:
                 file.writelines(todos)
 
         case "Exit":
